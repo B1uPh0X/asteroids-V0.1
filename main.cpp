19b7faa5f68a71ff2455326c530933433b96c773
shuckster87 <113372474+shuckster87@users.noreply.github.com>
2022-09-14 12:47:51 -0400

    Update main.cpp
    here's the updated version of the main program it includes:
    - deltatime 
    - deltatime implemented into movement
    - asteroids moving backwards
    - player collision into asteroids

--- main.cpp
+++ main.cpp
@@ -3,25 +3,17 @@
 #include <vector>
 
 int main() {
-    sf::RenderWindow window(sf::VideoMode(800, 500), "Testing");
+    sf::RenderWindow window(sf::VideoMode(800, 500), "Asteroids");
     sf::CircleShape player1(20, 3);
+    sf::Vector2f velocity(0, 0);
+    sf::Vector2f newPosition(0,0);
+    float acceleration = 1.f;
+
+
     player1.setFillColor(sf::Color::Red);
-    player1.move(rand() % 800, rand() % 500);
-    sf::CircleShape player2(20, 3);
-    player2.setFillColor(sf::Color::Blue);
-    player2.move(rand() % 800, rand() % 500);
-    sf::CircleShape movingasteroid1(20, 8);
-    movingasteroid1.setFillColor(sf::Color::White);
-    movingasteroid1.move(rand() % 800, rand() % 500);
-    sf::CircleShape movingasteroid2(20, 8);
-    movingasteroid2.setFillColor(sf::Color::White);
-    movingasteroid2.move(rand() % 800, rand() % 500);
-    sf::CircleShape movingasteroid3(20, 8);
-    movingasteroid3.setFillColor(sf::Color::White);
-    movingasteroid3.move(rand() % 800, rand() % 500);
-    sf::CircleShape movingasteroid4(20, 8);
-    movingasteroid4.setFillColor(sf::Color::White);
-    movingasteroid4.move(rand() % 800, rand() % 500);
+    player1.setRotation(90.f);
+    player1.move(0, 250);
+
     std::vector<sf::CircleShape> asteroids;
 
     for (int i = 0; i <= 14; i++) {
@@ -31,207 +23,56 @@ int main() {
         asteroids.push_back(asteroid);
     }
 
+
+
+    sf::Clock deltaclock;
+    window.setFramerateLimit(60);
     while (window.isOpen()) {
+        sf::Time deltatime = deltaclock.restart();
         sf::Event event;
         while (window.pollEvent(event)) {
-            if (event.type == sf::Event::Closed)
+            if (event.type == sf::Event::Closed) {
                 window.close();
-        }
-
-        window.clear();
-        for (unsigned int i = 0; i < asteroids.size(); i++) {
-            window.draw(asteroids.at(i));
-            if (player1.getGlobalBounds().intersects(asteroids[i].getGlobalBounds())) {
-
+            }
+            if(player1.getFillColor() == sf::Color::Transparent){
+                window.close();
             }
         }
+        
 
         window.draw(player1);
-        window.draw(player2);
-        window.draw(movingasteroid1);
-        window.draw(movingasteroid2);
-        window.draw(movingasteroid3);
-        window.draw(movingasteroid4);
         window.display();
-
-
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::W)) {
-            player1.move(0.f, -1 / 64.f);
-            if (player1.getGlobalBounds().intersects(player2.getGlobalBounds())) {
-                player1.move(0.f, 1 / 64.f);
+        window.clear();
+        for (unsigned int i = 0; i < asteroids.size(); i++) {
+            if(asteroids[i].getGlobalBounds().intersects(player1.getGlobalBounds())){
+               player1.setFillColor(sf::Color::Transparent);
             }
-            if (player1.getGlobalBounds().intersects(movingasteroid1.getGlobalBounds())) {
-                player1.move(0.f, 1 / 64.f);
-            }
-            if (player1.getGlobalBounds().intersects(movingasteroid2.getGlobalBounds())) {
-                player1.move(0.f, 1 / 64.f);
-            }
-            if (player1.getGlobalBounds().intersects(movingasteroid3.getGlobalBounds())) {
-                player1.move(0.f, 1 / 64.f);
-            }
-            if (player1.getGlobalBounds().intersects(movingasteroid4.getGlobalBounds())) {
-                player1.move(0.f, 1 / 64.f);
-            }
+            int random = rand() % asteroids.size();
+            asteroids[random].move(-1/4.f, 0.f);
+            window.draw(asteroids.at(i));
         }
 
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::S)) {
-            player1.move(0.f, 1 / 64.f);
-            if (player1.getGlobalBounds().intersects(player2.getGlobalBounds())) {
-                player1.move(0.f, -1 / 64.f);
-            }
-            if (player1.getGlobalBounds().intersects(movingasteroid1.getGlobalBounds())) {
-                player1.move(0.f, -1 / 64.f);
-            }
-            if (player1.getGlobalBounds().intersects(movingasteroid2.getGlobalBounds())) {
-                player1.move(0.f, -1 / 64.f);
-            }
-            if (player1.getGlobalBounds().intersects(movingasteroid3.getGlobalBounds())) {
-                player1.move(0.f, -1 / 64.f);
-            }
-            if (player1.getGlobalBounds().intersects(movingasteroid4.getGlobalBounds())) {
-                player1.move(0.f, -1 / 64.f);
-            }
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::D))
+        {
+            if(velocity.x < 10) velocity.x += acceleration * deltatime.asSeconds();
+            if(velocity.y < 10) velocity.y += acceleration * deltatime.asSeconds();
+            player1.move(1.f, 0.f);
+
         }
-
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::D)) {
-            player1.move(1 / 64.f, 0.f);
-            if (player1.getGlobalBounds().intersects(player2.getGlobalBounds())) {
-                player1.move(-1 / 64.f, 0.f);
-            }
-            if (player1.getGlobalBounds().intersects(movingasteroid1.getGlobalBounds())) {
-                player1.move(-1 / 64.f, 0.f);
-            }
-            if (player1.getGlobalBounds().intersects(movingasteroid2.getGlobalBounds())) {
-                player1.move(-1 / 64.f, 0.f);
-            }
-            if (player1.getGlobalBounds().intersects(movingasteroid3.getGlobalBounds())) {
-                player1.move(-1 / 64.f, 0.f);
-            }
-            if (player1.getGlobalBounds().intersects(movingasteroid4.getGlobalBounds())) {
-                player1.move(-1 / 64.f, 0.f);
-            }
+        if(sf::Keyboard::isKeyPressed(sf::Keyboard::A)){
+            if(velocity.x > 0) velocity.x += acceleration * deltatime.asSeconds();
+            if(velocity.y > 0) velocity.y += acceleration * deltatime.asSeconds();
+            player1.move(-1.f, 0.f);
         }
-
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::A)) {
-            player1.move(-1 / 64.f, 0.f);
-            if (player1.getGlobalBounds().intersects(player2.getGlobalBounds())) {
-                player1.move(1 / 64.f, 0.f);
-            }
-            if (player1.getGlobalBounds().intersects(movingasteroid1.getGlobalBounds())) {
-                player1.move(1 / 64.f, 0.f);
-            }
-            if (player1.getGlobalBounds().intersects(movingasteroid2.getGlobalBounds())) {
-                player1.move(1 / 64.f, 0.f);
-            }
-            if (player1.getGlobalBounds().intersects(movingasteroid3.getGlobalBounds())) {
-                player1.move(1 / 64.f, 0.f);
-            }
-            if (player1.getGlobalBounds().intersects(movingasteroid4.getGlobalBounds())) {
-                player1.move(1 / 64.f, 0.f);
-            }
+        if(sf::Keyboard::isKeyPressed(sf::Keyboard::W)){
+            player1.move(0.f, -1.f);
         }
-
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::I)) {
-            player2.move(0.f, -1 / 64.f);
-            if (player2.getGlobalBounds().intersects(player1.getGlobalBounds())) {
-                player2.move(0.f, 1 / 64.f);
-            }
-            if (player2.getGlobalBounds().intersects(movingasteroid1.getGlobalBounds())) {
-                player2.move(0.f, 1 / 64);
-            }
-            if (player2.getGlobalBounds().intersects(movingasteroid2.getGlobalBounds())) {
-                player2.move(0.f, 1 / 64);
-            }
-            if (player2.getGlobalBounds().intersects(movingasteroid3.getGlobalBounds())) {
-                player2.move(0.f, 1 / 64);
-            }
-            if (player2.getGlobalBounds().intersects(movingasteroid4.getGlobalBounds())) {
-                player2.move(0.f, 1 / 64);
-            }
+        if(sf::Keyboard::isKeyPressed(sf::Keyboard::S)){
+            player1.move(0.f, 1.f);
         }
-
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::K)) {
-            player2.move(0.f, 1 / 64.f);
-            if (player2.getGlobalBounds().intersects(player1.getGlobalBounds())) {
-                player2.move(0.f, -1 / 64.f);
-            }
-            if (player2.getGlobalBounds().intersects(movingasteroid1.getGlobalBounds())) {
-                player2.move(0.f, -1 / 64);
-            }
-            if (player2.getGlobalBounds().intersects(movingasteroid2.getGlobalBounds())) {
-                player2.move(0.f, -1 / 64);
-            }
-            if (player2.getGlobalBounds().intersects(movingasteroid3.getGlobalBounds())) {
-                player2.move(0.f, -1 / 64);
-            }
-            if (player2.getGlobalBounds().intersects(movingasteroid4.getGlobalBounds())) {
-                player2.move(0.f, -1 / 64);
-            }
-        }
-
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::L)) {
-            player2.move(1 / 64.f, 0.f);
-            if (player2.getGlobalBounds().intersects(player1.getGlobalBounds())) {
-                player2.move(-1 / 64.f, 0.f);
-            }
-            if (player2.getGlobalBounds().intersects(movingasteroid1.getGlobalBounds())) {
-                player2.move(-1 / 64.f, 0.f);
-            }
-            if (player2.getGlobalBounds().intersects(movingasteroid2.getGlobalBounds())) {
-                player2.move(-1 / 64.f, 0.f);
-            }
-            if (player2.getGlobalBounds().intersects(movingasteroid3.getGlobalBounds())) {
-                player2.move(-1 / 64.f, 0.f);
-            }
-            if (player2.getGlobalBounds().intersects(movingasteroid4.getGlobalBounds())) {
-                player2.move(-1 / 64.f, 0.f);
-            }
-        }
-
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::J)) {
-            player2.move(-1 / 64.f, 0.f);
-            if (player2.getGlobalBounds().intersects(player1.getGlobalBounds())) {
-                player2.move(1 / 64.f, 0.f);
-            }
-            if (player2.getGlobalBounds().intersects(movingasteroid1.getGlobalBounds())) {
-                player2.move(1 / 64.f, 0.f);
-            }
-            if (player2.getGlobalBounds().intersects(movingasteroid2.getGlobalBounds())) {
-                player2.move(1 / 64.f, 0.f);
-            }
-            if (player2.getGlobalBounds().intersects(movingasteroid3.getGlobalBounds())) {
-                player2.move(1 / 64.f, 0.f);
-            }
-            if (player2.getGlobalBounds().intersects(movingasteroid4.getGlobalBounds())) {
-                player2.move(1 / 64.f, 0.f);
-            }
-        }
-
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Escape)) {
-            window.close();
-        }
-
-
-        movingasteroid1.setRotation(90.f);
-        movingasteroid1.setOrigin(-300.f, 50.f);
-        movingasteroid1.move(-1 / 128.f, 0.f);
-
-        movingasteroid2.setRotation(90.f);
-        movingasteroid2.setOrigin(-300.f, 50.f);
-        movingasteroid2.move(-1 / 128.f, 0.f);
-
-        movingasteroid3.setRotation(90.f);
-        movingasteroid3.setOrigin(-300.f, 50.f);
-        movingasteroid3.move(-1 / 128.f, 0.f);
-
-        movingasteroid4.setRotation(90.f);
-        movingasteroid4.setOrigin(-300.f, 50.f);
-        movingasteroid4.move(-1 / 128.f, 0.f);
 
         player1.setRotation(90.f);
 
-        player2.setRotation(90.f);
     }
-
     return 0;
 }
