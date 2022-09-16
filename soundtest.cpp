62705bab3d480dfda285d3af99a24e62917b75ca
B1uPh0X <B1uPh0x@protonmail.com>
2022-09-16 14:29:49 -0400

    modified:   main.exe 	modified:   main.o 	modified:   soundtest.cpp 	new file:   soundtest.o

--- soundtest.cpp
+++ soundtest.cpp
@@ -1,21 +1,31 @@
 #include <SFML/Audio.hpp>
+#include <SFML/Window.hpp>
 #include <ctime>
 #include <iostream>
 
 void pause(int dur);
 
-int soundtest()
+int main()
 {
+
+    sf::Window window(sf::VideoMode(800,600), "My window");
+
     sf::SoundBuffer buffer;
-    if (!buffer.loadFromFile("sounds\\270306__littlerobotsoundfactory__explosion-02.wav"))
+    if (!buffer.loadFromFile("sounds\\explosion-02.wav"))
         return -1;
 
     sf::Sound sound;
     sound.setBuffer(buffer);
     sound.play();
     
-    pause(5);
-
+    while (window.isOpen()){
+        sf::Event event;
+        while (window.pollEvent(event)){
+            if (event.type == sf::Event::Closed){
+                    window.close();
+            }
+        }
+    }
     return 0;
 }
 
